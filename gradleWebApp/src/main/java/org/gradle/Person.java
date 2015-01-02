423e3198d13eae2d883d0629d9e6cee8512808ea
Alin Kovacs <alin.kovacs@gmail.com>
2015-01-02 22:49:21 +0100

    Inizializzato l'attributo surname

--- gradleWebApp/src/main/java/org/gradle/Person.java
+++ gradleWebApp/src/main/java/org/gradle/Person.java
@@ -4,7 +4,7 @@ import org.apache.commons.collections.list.GrowthList;
 
 public class Person {
     private final String name;
-    private final String surname;
+    private final String surname = null;
 
     public Person(String name) {
         this.name = name;
