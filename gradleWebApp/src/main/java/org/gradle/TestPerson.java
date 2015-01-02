60a59b08d53b451ab917b09d173cc8081c1ab799
Alin Kovacs <alin.kovacs@gmail.com>
2015-01-02 22:54:55 +0100

    Crato una nuova persona

--- gradleWebApp/src/main/java/org/gradle/TestPerson.java
+++ gradleWebApp/src/main/java/org/gradle/TestPerson.java
@@ -1,11 +1,13 @@
 package org.gradle;
 
+
 public class TestPerson {
 	
 	public static void main(String[] args) {
 		Person person = new Person("Igor");
+		Person marco = new Person("Marco");
 		
 		System.out.println("Ciao, io sono " + person.getName());
+		System.out.println("Salve, io mi chiamo " + marco.getName());
 	}
-
 }
